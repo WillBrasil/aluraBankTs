a02c53bac43420503bc9a8aa0319c1095cc58597
Willian Brasil <brasil-will@hotmail.com>
2020-12-06 14:02:17 -0300

    Primeiro Modulo custo TS completo

---
+++ ts/controllers/NegociacaoController.ts
@@ -0,0 +1,36 @@
+class NegociacaoController {
+
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView')
+
+    constructor() {
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+       // atualiza a view para exibir os dados do modelo, vazio
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            new Date(this._inputData.val().replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
+        );
+
+        this._negociacoes.adiciona(negociacao);
+
+        // depois de adicionar, atualiza a view novamente para refletir os dados
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
+}
+
